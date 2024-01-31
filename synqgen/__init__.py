8098fb94ceb0f62e62042d8629e2070ff527f54a
Tiago Almeida <tiagomeloalmeida@ua.pt>
2024-01-31 18:09:48 +0000

    version

--- synqgen/__init__.py
+++ synqgen/__init__.py
@@ -1,3 +1,5 @@
 """
 This module, SynQGen, provides tools for generating synthetic questions. It's designed to facilitate advanced question generation for AI and NLP applications.
 """
+
+__version__=="0.0.1"
